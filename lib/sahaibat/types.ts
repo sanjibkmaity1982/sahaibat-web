17a0559487e5c355aff12b3535660e76294b4ead
sanjibkmaity1982 <sanjibkmaity1982@gmail.com>
2026-01-25 03:40:59 -0500

    Update types.ts

--- lib/sahaibat/types.ts
+++ lib/sahaibat/types.ts
@@ -1,1 +1,11 @@
+export type Scenario = {
+  id: string;
+  title: string;
+  subtitle: string;
+  chw: string[];
+  reviewer: string[];
+  patient: string[];
+};
+
+export type RoleKey = "chw" | "reviewer" | "ngo";
 
